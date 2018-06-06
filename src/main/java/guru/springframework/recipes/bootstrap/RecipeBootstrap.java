363900e28b3ea7903e264696e859c1ac12e5fa4a
Jaime Alvarez <jaimeandresalvarez@gmail.com>
2018-06-06 00:01:54 -0500

    Se incluye compatibilidad con mysql

--- src/main/java/guru/springframework/recipes/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/recipes/bootstrap/RecipeBootstrap.java
@@ -6,6 +6,7 @@ import guru.springframework.recipes.repositories.RecipeRepository;
 import guru.springframework.recipes.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
